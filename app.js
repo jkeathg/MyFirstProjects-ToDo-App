1ab85323802e71aaaa92317d726abe05437d34d1
jkeathg <jkeathg@hotmail.comgit config --listgit config --global user.name Jeremy>
2022-03-29 21:37:16 -0400

    ToDo input and buttons created. ability to push submitted todo empty div li elements function. Things to work on: -appending multiple todo's - adding finish and clear buttons - making main clear button work

--- app.js
+++ app.js
@@ -7,3 +7,21 @@
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+
+const addToDo = document.getElementById("addToDo").value;
+const submitToDo = document.getElementById("submitToDo");
+
+const listToDo = document.getElementById("listDiv");
+const oList = document.createElement("ol");
+
+const liToDo = document.createElement("li");
+liToDo.appendChild(document.createTextNode(addToDo));
+oList.appendChild(liToDo);
+
+
+
+
+submitToDo.addEventListener("click",() => {
+    
+    listToDo.appendChild(oList);
+});
